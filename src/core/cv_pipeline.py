2bac2458999ccab9012a76b6bd343cd4cf4df0f2
InezTech <info@ineztech.eu>
2026-02-27 22:56:44 +0100

    fix: test with python 3.10 to avoid mediapipe wheel issues on linux ci

--- src/core/cv_pipeline.py
+++ src/core/cv_pipeline.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-import mediapipe.python.solutions.hands as mp_hands
 import math
 
 class CVPipeline:
     def __init__(self):
-        self.hands = mp_hands.Hands(
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
             static_image_mode=False,
             max_num_hands=1,
             min_detection_confidence=0.7,
